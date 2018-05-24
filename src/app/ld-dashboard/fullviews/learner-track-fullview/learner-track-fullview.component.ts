89c56ff5080e287839555d4f7f50fa17b5249e67
praveenkondani <praveenaprdc@gmail.com>
2018-05-24 10:29:12 +0530

    css

--- src/app/ld-dashboard/fullviews/learner-track-fullview/learner-track-fullview.component.ts
+++ src/app/ld-dashboard/fullviews/learner-track-fullview/learner-track-fullview.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-learner-track-fullview',
   templateUrl: './learner-track-fullview.component.html',
-  styleUrls: ['./learner-track-fullview.component.css']
+  styleUrls: ['./learner-track-fullview.component.scss']
 })
 export class LearnerTrackFullviewComponent implements OnInit {
 

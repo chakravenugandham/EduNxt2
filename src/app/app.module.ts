f02d93493215c32999d309c6a545ef93305a522c
praveenkondani <praveenaprdc@gmail.com>
2018-05-31 19:06:52 +0530

    world cloud start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,68 +1,69 @@
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { HttpClientModule } from "@angular/common/http";
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { NvD3Module } from 'ng2-nvd3';
-import { Daterangepicker } from 'ng2-daterangepicker';
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { Daterangepicker } from "ng2-daterangepicker";
 
 // d3 and nvd3 should be included somewhere
-import 'd3';
-import 'nvd3';
+import { NvD3Module } from "ng2-nvd3";
+import "d3";
+import "nvd3";
+
+//word cloud moudle
+import { TagCloudComponent } from "angular-tag-cloud-module";
 
 //modules
-import { RoutingModule } from './routing.module';
+import { RoutingModule } from "./routing.module";
 
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
-import { TimeFrameComponent } from './common/time-frame/time-frame.component';
-import { LdDashboardComponent } from './ld-dashboard/ld-dashboard.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./common/header/header.component";
+import { TimeFrameComponent } from "./common/time-frame/time-frame.component";
+import { LdDashboardComponent } from "./ld-dashboard/ld-dashboard.component";
 import { ActivityWidgetComponent } from "./ld-dashboard/activity-widget/activity-widget.component";
-import { UsersComponent } from './ld-dashboard/activity-widget/users/users.component';
-import { EngagementComponent } from './ld-dashboard/activity-widget/engagement/engagement.component';
-import { PaceComponent } from './ld-dashboard/activity-widget/pace/pace.component';
-import { FeedbackComponent } from './ld-dashboard/activity-widget/feedback/feedback.component';
-import { ActiveUserWidgetComponent } from './ld-dashboard/active-user-widget/active-user-widget.component';
-import { ActiveUsersComponent } from './ld-dashboard/active-user-widget/active-users/active-users.component';
-import { ModeOfDeliveryComponent } from './ld-dashboard/active-user-widget/mode-of-delivery/mode-of-delivery.component';
-import { LocationComponent } from './ld-dashboard/active-user-widget/location/location.component';
-import { LearnersTrackWidgetComponent } from './ld-dashboard/learners-track-widget/learners-track-widget.component';
-import { LearnerPaceComponent } from './ld-dashboard/learners-track-widget/learner-pace/learner-pace.component';
-import { LearnerPerformanceComponent } from './ld-dashboard/learners-track-widget/learner-performance/learner-performance.component';
-import { LearnersPerformanceWidgetComponent } from './ld-dashboard/learners-performance-widget/learners-performance-widget.component';
-import { PerformanceComponent } from './ld-dashboard/learners-performance-widget/performance/performance.component';
-import { ProgressComponent } from './ld-dashboard/learners-performance-widget/progress/progress.component';
-import { ScoresDistributionWidgetComponent } from './ld-dashboard/scores-distribution-widget/scores-distribution-widget.component';
-import { TestScoresComponent } from './ld-dashboard/scores-distribution-widget/test-scores/test-scores.component';
-import { QuizComponent } from './ld-dashboard/scores-distribution-widget/quiz/quiz.component';
-import { AssignmentComponent } from './ld-dashboard/scores-distribution-widget/assignment/assignment.component';
-import { OrgPerformanceWidgetComponent } from './ld-dashboard/org-performance-widget/org-performance-widget.component';
-import { TeamsComponent } from './ld-dashboard/org-performance-widget/teams/teams.component';
-import { TrainersComponent } from './ld-dashboard/org-performance-widget/trainers/trainers.component';
-import { LearnersComponent } from './ld-dashboard/org-performance-widget/learners/learners.component';
-import { ContentPerformanceWidgetComponent } from './ld-dashboard/content-performance-widget/content-performance-widget.component';
-import { ContentConsumptionComponent } from './ld-dashboard/content-performance-widget/content-consumption/content-consumption.component';
-import { NotificationPerformanceWidgetComponent } from './ld-dashboard/notification-performance-widget/notification-performance-widget.component';
-import { ScheduledDeliveredComponent } from './ld-dashboard/notification-performance-widget/scheduled-delivered/scheduled-delivered.component';
-import { SeenRespondedComponent } from './ld-dashboard/notification-performance-widget/seen-responded/seen-responded.component';
-import { OrgInterestWidgetComponent } from './ld-dashboard/org-interest-widget/org-interest-widget.component';
-import { OrgInterestComponent } from './ld-dashboard/org-interest-widget/org-interest/org-interest.component';
-import { AttentionNeedWidgetComponent } from './ld-dashboard/attention-need-widget/attention-need-widget.component';
-import { LearnerTrackFullviewComponent } from './ld-dashboard/fullviews/learner-track-fullview/learner-track-fullview.component';
-import { MpDatepickerRangeComponent } from './common/mp-datepicker-range/mp-datepicker-range.component';
-import { ContentConsumptionFullviewComponent } from './ld-dashboard/fullviews/content-consumption-fullview/content-consumption-fullview.component';
-import { FooterComponent } from './common/footer/footer.component';
-import { TimespentComponent } from './ld-dashboard/activity-widget/timespent/timespent.component';
-import { UserstrainedComponent } from './ld-dashboard/activity-widget/userstrained/userstrained.component';
-import { FilterWidgetComponent } from './ld-dashboard/shared/filter-widget/filter-widget.component';
-import { LearnerPerformanceFullviewComponent } from './ld-dashboard/fullviews/learner-performance-fullview/learner-performance-fullview.component';
-import { ScoresDistributionFullviewComponent } from './ld-dashboard/fullviews/scores-distribution-fullview/scores-distribution-fullview.component';
-import { OrgPerformanceFullviewComponent } from './ld-dashboard/fullviews/org-performance-fullview/org-performance-fullview.component';
-import { NotificationPerformanceFullviewComponent } from './ld-dashboard/fullviews/notification-performance-fullview/notification-performance-fullview.component';
-import { OrgInterestFullviewComponent } from './ld-dashboard/fullviews/org-interest-fullview/org-interest-fullview.component';
+import { UsersComponent } from "./ld-dashboard/activity-widget/users/users.component";
+import { EngagementComponent } from "./ld-dashboard/activity-widget/engagement/engagement.component";
+import { PaceComponent } from "./ld-dashboard/activity-widget/pace/pace.component";
+import { FeedbackComponent } from "./ld-dashboard/activity-widget/feedback/feedback.component";
+import { ActiveUserWidgetComponent } from "./ld-dashboard/active-user-widget/active-user-widget.component";
+import { ActiveUsersComponent } from "./ld-dashboard/active-user-widget/active-users/active-users.component";
+import { ModeOfDeliveryComponent } from "./ld-dashboard/active-user-widget/mode-of-delivery/mode-of-delivery.component";
+import { LocationComponent } from "./ld-dashboard/active-user-widget/location/location.component";
+import { LearnersTrackWidgetComponent } from "./ld-dashboard/learners-track-widget/learners-track-widget.component";
+import { LearnerPaceComponent } from "./ld-dashboard/learners-track-widget/learner-pace/learner-pace.component";
+import { LearnerPerformanceComponent } from "./ld-dashboard/learners-track-widget/learner-performance/learner-performance.component";
+import { LearnersPerformanceWidgetComponent } from "./ld-dashboard/learners-performance-widget/learners-performance-widget.component";
+import { PerformanceComponent } from "./ld-dashboard/learners-performance-widget/performance/performance.component";
+import { ProgressComponent } from "./ld-dashboard/learners-performance-widget/progress/progress.component";
+import { ScoresDistributionWidgetComponent } from "./ld-dashboard/scores-distribution-widget/scores-distribution-widget.component";
+import { TestScoresComponent } from "./ld-dashboard/scores-distribution-widget/test-scores/test-scores.component";
+import { QuizComponent } from "./ld-dashboard/scores-distribution-widget/quiz/quiz.component";
+import { AssignmentComponent } from "./ld-dashboard/scores-distribution-widget/assignment/assignment.component";
+import { OrgPerformanceWidgetComponent } from "./ld-dashboard/org-performance-widget/org-performance-widget.component";
+import { TeamsComponent } from "./ld-dashboard/org-performance-widget/teams/teams.component";
+import { TrainersComponent } from "./ld-dashboard/org-performance-widget/trainers/trainers.component";
+import { LearnersComponent } from "./ld-dashboard/org-performance-widget/learners/learners.component";
+import { ContentPerformanceWidgetComponent } from "./ld-dashboard/content-performance-widget/content-performance-widget.component";
+import { ContentConsumptionComponent } from "./ld-dashboard/content-performance-widget/content-consumption/content-consumption.component";
+import { NotificationPerformanceWidgetComponent } from "./ld-dashboard/notification-performance-widget/notification-performance-widget.component";
+import { ScheduledDeliveredComponent } from "./ld-dashboard/notification-performance-widget/scheduled-delivered/scheduled-delivered.component";
+import { SeenRespondedComponent } from "./ld-dashboard/notification-performance-widget/seen-responded/seen-responded.component";
+import { OrgInterestWidgetComponent } from "./ld-dashboard/org-interest-widget/org-interest-widget.component";
+import { OrgInterestComponent } from "./ld-dashboard/org-interest-widget/org-interest/org-interest.component";
+import { AttentionNeedWidgetComponent } from "./ld-dashboard/attention-need-widget/attention-need-widget.component";
+import { LearnerTrackFullviewComponent } from "./ld-dashboard/fullviews/learner-track-fullview/learner-track-fullview.component";
+import { MpDatepickerRangeComponent } from "./common/mp-datepicker-range/mp-datepicker-range.component";
+import { ContentConsumptionFullviewComponent } from "./ld-dashboard/fullviews/content-consumption-fullview/content-consumption-fullview.component";
+import { FooterComponent } from "./common/footer/footer.component";
+import { TimespentComponent } from "./ld-dashboard/activity-widget/timespent/timespent.component";
+import { UserstrainedComponent } from "./ld-dashboard/activity-widget/userstrained/userstrained.component";
+import { FilterWidgetComponent } from "./ld-dashboard/shared/filter-widget/filter-widget.component";
+import { LearnerPerformanceFullviewComponent } from "./ld-dashboard/fullviews/learner-performance-fullview/learner-performance-fullview.component";
+import { ScoresDistributionFullviewComponent } from "./ld-dashboard/fullviews/scores-distribution-fullview/scores-distribution-fullview.component";
+import { OrgPerformanceFullviewComponent } from "./ld-dashboard/fullviews/org-performance-fullview/org-performance-fullview.component";
+import { NotificationPerformanceFullviewComponent } from "./ld-dashboard/fullviews/notification-performance-fullview/notification-performance-fullview.component";
+import { OrgInterestFullviewComponent } from "./ld-dashboard/fullviews/org-interest-fullview/org-interest-fullview.component";
 
 @NgModule({
   declarations: [
@@ -112,7 +113,8 @@ import { OrgInterestFullviewComponent } from './ld-dashboard/fullviews/org-inter
     ScoresDistributionFullviewComponent,
     OrgPerformanceFullviewComponent,
     NotificationPerformanceFullviewComponent,
-    OrgInterestFullviewComponent
+    OrgInterestFullviewComponent,
+    TagCloudComponent
   ],
   imports: [
     BrowserModule,
@@ -126,4 +128,4 @@ import { OrgInterestFullviewComponent } from './ld-dashboard/fullviews/org-inter
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

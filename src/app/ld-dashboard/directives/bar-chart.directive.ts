d1b82197a2e3fa159de13bb35bd9db4f247c12c7
praveenkondani <praveenaprdc@gmail.com>
2018-07-17 17:16:34 +0530

    performance progress design change

--- src/app/ld-dashboard/directives/bar-chart.directive.ts
+++ src/app/ld-dashboard/directives/bar-chart.directive.ts
@@ -6,14 +6,28 @@ declare let d3: any;
 @Directive({
   selector: "[appBarChart]"
 })
-export class BarChartDirective implements OnChanges {
+export class BarChartDirective implements OnInit, OnChanges {
   @Input() data;
-  dataset = [];
+  // dataset = [];
 
   // dataset model
-  // dataset = [{ label: "course_name", Group1: 60, Group2: 72, Group3: 46 }];
+  dataset = [
+    { label: "Data Structures", Group1: 60 },
+    { label: "Algo", Group1: 30 },
+    { label: "Database", Group1: 40 },
+    { label: "ML", Group1: 50 },
+    { label: "Problem Solving", Group1: 60 },
+    { label: "DB 2", Group1: 30 },
+    { label: "D S 2", Group1: 20 },
+    { label: "Algorithms1", Group1: 70 },
+    { label: "ML 2", Group1: 40 },
+    { label: "Problem", Group1: 50 },
+    { label: "ML 3", Group1: 30 },
+    { label: "Database 3", Group1: 80 },
+    { label: "Problem Solving4", Group1: 60 }
+  ];
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   performanceChart() {
     this.el.nativeElement.innerHTML = "";
@@ -57,16 +71,29 @@ export class BarChartDirective implements OnChanges {
         .node()
         .getBoundingClientRect().width,
       height = 200;
+    let calculatedWidth =
+      this.dataset.length > 6 ? width + 46 * (this.dataset.length - 6) : width;
+
+    if (this.dataset.length > 6) {
+      d3.select(".bar-chart-graph").attr("overflow-x", "scroll");
+    }
+
+    console.log("width", width);
+    console.log("this.dataset.length", this.dataset.length);
+    console.log("this.dataset.length / 6", this.dataset.length - 6);
+
+    console.log("calculatedWidth", calculatedWidth);
 
     let svg = d3
       .select(this.el.nativeElement)
       .append("svg")
-      .attr("width", width)
+      // .attr("min-width", width)
+      .attr("width", calculatedWidth)
       .attr("height", height + margin * 2)
       .append("g")
       .attr("transform", "translate(" + margin + "," + margin + ")");
 
-    let x0 = d3.scale.ordinal().rangeRoundBands([0, width], 0.5, 0.5);
+    let x0 = d3.scale.ordinal().rangeRoundBands([0, calculatedWidth], 0.5, 0.5);
 
     let x1 = d3.scale.ordinal();
 
@@ -83,18 +110,20 @@ export class BarChartDirective implements OnChanges {
       .orient("left")
       .tickFormat(d3.format(".2s"));
 
-    let options = d3.keys(this.dataset[0]).filter(function (key) {
+    let options = d3.keys(this.dataset[0]).filter(function(key) {
       return key !== "label";
     });
 
-    this.dataset.forEach(function (d) {
-      d.valores = options.map(function (name) {
+    this.dataset.forEach(function(d: any) {
+      // console.log("d", d);
+
+      d.valores = options.map(function(name) {
         return { name: name, value: +d[name] };
       });
     });
 
     x0.domain(
-      this.dataset.map(function (d) {
+      this.dataset.map(function(d) {
         return d.label;
       })
     );
@@ -123,49 +152,54 @@ export class BarChartDirective implements OnChanges {
       .enter()
       .append("g")
       .attr("class", "rect")
-      .attr("transform", function (d) {
+      .attr("transform", function(d) {
         return "translate(" + x0(d.label) + ",0)";
       });
 
     let color = d3.scale
       .ordinal()
-      .range(["#FFD630", "#F77F6C", "#5584FF", "#23b14d"]);
+      .range(["#F77F6C", "#FFD630", "#5584FF", "#23b14d"]);
 
-    svg.append("text")
-      .text("No.of users")
+    svg
+      .append("text")
+      .text("Performance")
       .attr("transform", "rotate(-90)")
-      .attr("x", - (height / 2))
-      .attr("y", 20)
+      .attr("x", -(height / 2))
+      .attr("y", 20);
 
     bar
       .selectAll("rect")
-      .data(function (d) {
+      .data(function(d) {
         return d.valores;
       })
       .enter()
       .append("rect")
-      .attr("width", x1.rangeBand() - 8)
-      .attr("x", function (d) {
+      // .attr("width", x1.rangeBand() - 8)
+      .attr("width", 24)
+      .attr("x", function(d) {
         return x1(d.name);
       })
-      .attr("y", function (d) {
+      .attr("y", function(d) {
         return y(d.value);
       })
-      .attr("value", function (d) {
+      .attr("value", function(d) {
         return d.name;
       })
-      .attr("height", function (d) {
+      .attr("height", function(d) {
         return height - y(d.value);
       })
-      .style("fill", function (d) {
+      .style("fill", function(d) {
         return color(d.name);
       });
   }
 
+  ngOnInit() {
+    this.performanceChart();
+  }
   ngOnChanges(changes: any) {
     if (changes.data && changes.data.currentValue) {
-      this.dataset = this.data;
-      this.performanceChart();
+      // this.dataset = this.data;
+      // this.performanceChart();
     }
   }
 }

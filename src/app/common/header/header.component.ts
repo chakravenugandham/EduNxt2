8edd72adf912480c141ca2107b7dd9186d06e6a3
praveenkondani <praveenaprdc@gmail.com>
2018-08-31 15:22:18 +0530

    auth token integration

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -3,7 +3,7 @@ import { CommonService } from "../../common-services/common.service";
 import { LdDashboardService } from "../../ld-dashboard/services/ld-dashboard.service";
 
 import { CookieService } from 'ngx-cookie-service';
-
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -14,24 +14,36 @@ export class HeaderComponent implements OnInit {
   selectTenantName: string = "MAIT";
   tenantsName = ["MAIT", "MAB", "HDFC", "SMUDE", "PROLEARN"];
 
-  userName = 'User'
+  userName = (this.cookieService.get('user_name') == '') ? 'User' : this.cookieService.get('user_name');
+  tenantName = (this.cookieService.get('tenantName') == '') ? 'MAIT' : this.cookieService.get('tenantName');
+  redirectUrl = (this.cookieService.get('redirectUrl') == '') ? '/' : this.cookieService.get('redirectUrl');
+  user_id = (this.cookieService.get('user_id') == '') ? '0' : this.cookieService.get('user_id');
+  cookieFound = (this.cookieService.get('user_name') == '') ? false : true;
+
   // userName = this.cookieService.get('user_name');
-  cookieFound = false;
+  // cookieFound = true;
 
   constructor(
     private dashboardService: LdDashboardService,
-    private cookieService: CookieService
-  ) { }
+    private cookieService: CookieService,
+    private router: Router
+  ) {
+
+  }
 
   changeCourse(selectTenantName) {
-    this.dashboardService.selectTenantName(selectTenantName);
+    //this.dashboardService.selectTenantName(selectTenantName);
   }
 
   logout() {
-    this.dashboardService.logout().subscribe(res => console.log(res));
+    //this.dashboardService.logout().subscribe(res => console.log(res));
+    //this.router.navigate();
+    localStorage.removeItem('t');
+    window.location.href = this.redirectUrl;
   }
 
   ngOnInit() {
-    // this.cookieService.set('userName', "Praveen Kondani");
+    //this.cookieService.set('user_name', "Praveen Kondani");
+    this.dashboardService.selectTenantNameV2(this.tenantName, this.user_id);
   }
 }

878ee1d9549243fb20f9882a9894301aba8a26f8
praveenkondani <praveenaprdc@gmail.com>
2018-09-18 15:52:47 +0530

    environment urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl: "http://ec2-13-251-23-36.ap-southeast-1.compute.amazonaws.com:8080/api/v1/lnd/",
+  baseUrl: "http://ec2-13-251-23-36.ap-southeast-1.compute.amazonaws.com:8080/api/v1/lnd/",
   // baseUrl: 'http://172.24.1.53:8080/api/v1/lnd/',
-  baseUrl: 'http://192.168.239.38:8080/api/v1/lnd/',
   logoutUrl: 'http://ec2-13-251-23-36.ap-southeast-1.compute.amazonaws.com:8080/logout'
   // logoutUrl: 'http://http://172.24.1.53:8080/logout'
 };
